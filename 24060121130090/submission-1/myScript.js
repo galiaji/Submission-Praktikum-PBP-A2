78c45ba3b989bf41e765171c9d2ea629b0993761
dorinobaharson <89585868+dorinobaharson@users.noreply.github.com>
2023-09-04 14:46:53 +0700

    Add files via upload

---
+++ 24060121130090/submission-1/myScript.js
@@ -0,0 +1,10 @@
+
+// Nama : Dorino Baharson
+// NIM  :24060121130090
+// lab  : PBP A2
+// Tanggal : 1/9/2023
+// Deskripsi : Pembelajaran dasar javascript pada HTML
+
+function myFunction() {
+    document.getElementById("paragraf").innerHTML = "Hyper Text Markup Language";
+    }
